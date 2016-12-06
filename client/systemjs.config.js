7592a53cf9ab2219a67c118a67c0bdb3b55ba44f
Kaciano Ghelere <kaciano_ghelere@yahoo.com.br>
2016-12-06 17:33:55 -0200

    Ajuste PRD

--- client/systemjs.config.js
+++ client/systemjs.config.js
@@ -30,7 +30,7 @@
     packages: {
       app: {
         main: 'main.js',
-        defaultExtension: 'js'
+        defaultExtension: false
       },
       rxjs: { defaultExtension: false },
       'angular2-in-memory-web-api': {
